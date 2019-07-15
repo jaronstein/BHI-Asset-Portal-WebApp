7f4732807f7c76e6fb9ebc9c0ee61fa586691225
jaronstein <jaronstein@gmail.com>
2019-07-15 16:49:24 -0500

    Add project files.

---
+++ BHI Asset Portal WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BHI_Asset_Portal_WebApp.MvcApplication" Language="C#" %>
---
+++ BHI Asset Portal WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BHI_Asset_Portal_WebApp.Startup))]
+namespace BHI_Asset_Portal_WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
